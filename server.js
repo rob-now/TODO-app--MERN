bdae6d7219c64d27d41e5e271a2bd3222422afed
rob-now <rnowakowski.prv@gmail.com>
2018-07-12 16:24:16 +0200

    Initial server setup

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var app = express()
+
+var port = process.env.PORT || 3000
+
+app.set('view engine', 'ejs')
+
+app.use('/assets', express.static(__dirname + '/public'))
+
+app.listen(port)
