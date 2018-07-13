f91b56ac86cada7185e6cb26aae1d7eed329e606
rob-now <rnowakowski.prv@gmail.com>
2018-07-13 09:45:17 +0200

    Initial server config

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 var express = require('express')
 var app = express()
 
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 5000
 
 app.set('view engine', 'ejs')
 
 app.use('/assets', express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => res.send('Hello!'))
+
+app.get('/api', (req, res) => {
+  var obj = [
+    {
+      id: 0,
+      name: 'John Doe'
+    },
+    {
+      id: 1,
+      name: 'Jane Doe'
+    },
+    {
+      id: 2,
+      name: 'Jim Doe'
+    }
+  ]
+
+  res.json(obj)
+
+})
 
 app.listen(port)
