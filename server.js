472cfba4bc863ae5b8397825d7d3e0efc3f47cc6
rob-now <rnowakowski.prv@gmail.com>
2018-07-16 20:14:17 +0200

    Using apiController function in server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const config = require('./config')
 const setupController = require('./controllers/setupController')
+const apiController = require('./controllers/apiController')
 
 const app = express()
 
@@ -13,26 +14,6 @@ app.use('/assets', express.static(`${__dirname}/public`))
 
 mongoose.connect(config.getDbConnectionString())
 setupController(app)
-
-app.get('/', (req, res) => res.send('Hello!'))
-
-app.get('/api', (req, res) => {
-  const obj = [
-    {
-      id: 0,
-      name: 'John Doe',
-    },
-    {
-      id: 1,
-      name: 'Jane Doe',
-    },
-    {
-      id: 2,
-      name: 'Jim Doe',
-    },
-  ]
-
-  res.json(obj)
-})
+apiController(app)
 
 app.listen(port)
