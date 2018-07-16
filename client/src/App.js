2a878728ac52bc3d28bd3a9d1a06875bdb87a2e2
rob-now <rnowakowski.prv@gmail.com>
2018-07-16 20:43:15 +0200

    App.js changed to stateless component

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-class App extends Component {
-  state = {
-    people: [],
-  }
-
-  componentDidMount() {
-    fetch('/api')
-      .then(res => res.json())
-      .then(people => this.setState({ people }))
-  }
-
-  render() {
-    const { people } = this.state
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <h2>People</h2>
-        <ul>
-          {
-            people.map(person => (
-              <li
-                key={person.id}
-              >
-                {person.name}
-              </li>
-            ))
-          }
-        </ul>
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+      <h2>People</h2>
+    </div>
+  )
 }
 
 export default App
