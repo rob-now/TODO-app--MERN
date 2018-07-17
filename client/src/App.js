8d4ddda04edae9b1f5ac15bab89ca61139246111
rob-now <rnowakowski.prv@gmail.com>
2018-07-17 12:53:45 +0200

    Fetching todos from our API

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Welcome to React</h1>
-      </header>
-      <h2>People</h2>
-    </div>
-  )
+class App extends Component {
+  state = {
+    todos: [],
+  }
+
+  componentDidMount() {
+    fetch('/api/todos/test_user1')
+      .then(res => res.json())
+      .then(todos => this.setState({ todos }))
+  }
+
+  render() {
+    const { todos } = this.state
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <h2>Todos</h2>
+        <ul>
+          {
+            todos && todos.map(todo => (
+              <li key={todo._id}>
+                {todo.todo} [is done: {todo.isDone.toString()}]
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    )
+  }
 }
 
 export default App
