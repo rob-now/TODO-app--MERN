e72ec42997ccbd93d5a076432081c8bf2ae548e0
rob-now <rnowakowski.prv@gmail.com>
2018-07-17 17:47:19 +0200

    Added remove Todo

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,23 @@ class App extends Component {
       .then(todos => this.setState({ todos }))
   }
 
+  removeTodo = (todoId) => {
+    this.setState(({ todos }) => ({
+      todos: todos.filter(todo => (
+        todo._id !== todoId)),
+    }))
+
+    fetch('/api/todo', {
+      method: 'DELETE',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        id: todoId,
+      }),
+    })
+  }
+
   render() {
     const { todos } = this.state
     return (
@@ -27,6 +44,12 @@ class App extends Component {
             todos && todos.map(todo => (
               <li key={todo._id}>
                 {todo.todo} [is done: {todo.isDone.toString()}]
+                <button
+                  type="button"
+                  onClick={() => this.removeTodo(todo._id)}
+                >
+                  Remove
+                </button>
               </li>
             ))
           }
