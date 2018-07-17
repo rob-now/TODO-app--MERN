7327c86ffa03a19298e992504d9be66377a64e9e
rob-now <rnowakowski.prv@gmail.com>
2018-07-17 18:26:09 +0200

    Adding new todo

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import './App.css'
 
 class App extends Component {
   state = {
+    todoName: '',
     todos: [],
   }
 
@@ -11,6 +12,42 @@ class App extends Component {
     fetch('/api/todos/test_user1')
       .then(res => res.json())
       .then(todos => this.setState({ todos }))
+      .catch(err => err.message)
+  }
+
+  handleChange = (event) => {
+    event.preventDefault()
+
+    this.setState({
+      [event.target.name]: event.target.value,
+    })
+  }
+
+  addTodo = (event) => {
+    event.preventDefault()
+
+    const { todoName } = this.state
+
+    fetch('api/todo', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: 'test-user1',
+        todo: todoName,
+        isDone: 'false',
+        hasAttachment: 'false',
+      }),
+    }).catch(err => err.message)
+      .then(fetch('/api/todos/test_user1')
+        .then(res => res.json())
+        .then(todos => this.setState({ todos }))
+        .catch(err => err.message))
+
+    this.setState({
+      todoName: '',
+    })
   }
 
   removeTodo = (todoId) => {
@@ -31,7 +68,7 @@ class App extends Component {
   }
 
   render() {
-    const { todos } = this.state
+    const { todos, todoName } = this.state
     return (
       <div className="App">
         <header className="App-header">
@@ -39,6 +76,22 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <h2>Todos</h2>
+        <form onSubmit={this.addTodo}>
+          <label htmlFor="form-todo-name">Todo name:
+            <input
+              id="form-todo-name"
+              type="text"
+              name="todoName"
+              value={todoName}
+              onChange={this.handleChange}
+            />
+          </label>
+          <button
+            type="submit"
+          >
+            Add todo
+          </button>
+        </form>
         <ul>
           {
             todos && todos.map(todo => (
