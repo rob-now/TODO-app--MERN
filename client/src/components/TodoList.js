760c2faaa956484453ac326502e68d6c23b2bfb9
rob-now <rnowakowski.prv@gmail.com>
2018-07-20 10:50:36 +0200

    Changed to class component with state, added edit Todo button and displaying conditionally asterisk when in edit mode

--- client/src/components/TodoList.js
+++ client/src/components/TodoList.js
@@ -1,41 +1,65 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function TodoList(props) {
-  const {
-    todos,
-    isFetching,
-    removeTodo,
-    toggleTodoDone,
-  } = props
+class TodoList extends Component {
+  state = {
+    editTodoId: null,
+  }
 
-  return (
-    <ul>
-      {
-        isFetching
-          ? <h3>Loading Todos...</h3>
-          : todos.map(todo => (
-            <li key={todo._id}>
-              <input
-                type="checkbox"
-                checked={todo.isDone}
-                onChange={() => toggleTodoDone(todo._id, todo)}
-              />
-              {
-                todo.isDone
-                  ? <del>{todo.todo}</del>
-                  : todo.todo
-              }
-              <button
-                type="button"
-                onClick={() => removeTodo(todo._id)}
-              >
-                Remove
-              </button>
-            </li>
-          ))
-      }
-    </ul>
-  )
+  editTodoMode = (todoId) => {
+    this.setState({
+      editTodoId: todoId,
+    })
+  }
+
+
+  render() {
+    const {
+      todos,
+      isFetching,
+      removeTodo,
+      toggleTodoDone,
+    } = this.props
+
+    const { editTodoId } = this.state
+    console.log(editTodoId)
+    return (
+      <ul>
+        {
+          isFetching
+            ? <h3>Loading Todos...</h3>
+            : todos.map(todo => (
+              <li key={todo._id}>
+                <input
+                  type="checkbox"
+                  checked={todo.isDone}
+                  onChange={() => toggleTodoDone(todo._id, todo)}
+                />
+                {
+                  editTodoId === todo._id && '*'
+                }
+                {
+                  todo.isDone
+                    ? <del>{todo.todo}</del>
+                    : todo.todo
+                }
+                <button
+                  type="button"
+                  onClick={() => this.editTodoMode(todo._id)}
+                >
+                  Edit
+                </button>
+                <button
+                  type="button"
+                  onClick={() => removeTodo(todo._id)}
+                >
+                  Remove
+                </button>
+              </li>
+            ))
+        }
+      </ul>
+    )
+  }
 }
 
 export default TodoList
